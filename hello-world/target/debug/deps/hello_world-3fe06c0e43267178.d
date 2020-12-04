935ccdf71e041d33fbecd44f738eda49aa8d8372
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2020-12-04 12:33:17 -0300

    hello world

---
+++ hello-world/target/debug/deps/hello_world-3fe06c0e43267178.d
@@ -0,0 +1,5 @@
+D:\GitHub\Learning-Rust\hello-world\target\debug\deps\hello_world-3fe06c0e43267178.exe: src\main.rs
+
+D:\GitHub\Learning-Rust\hello-world\target\debug\deps\hello_world-3fe06c0e43267178.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+D:\GitHub\Learning-Rust\hello-world\target\debug\hello-world.exe: D:\GitHub\Learning-Rust\hello-world\src\main.rs
