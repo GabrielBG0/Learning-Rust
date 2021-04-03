985eb3e339da1d21bfde93340139103ba8256845
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-04-02 22:05:53 -0300

    rotating triangle

--- opengl/src/moving_triangle.rs
+++ opengl/src/moving_triangle.rs
@@ -1,4 +1,4 @@
-use glium::{Surface, implement_vertex, Program, Display, uniform};
+use glium::{implement_vertex, uniform, Display, Program, Surface};
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
@@ -11,34 +11,37 @@ struct Vertex {
 
 implement_vertex!(Vertex, position);
 
-pub fn create_moving_triangle(){
+pub fn create_moving_triangle() {
     let mut event_loop = EventLoop::new();
     let wb = WindowBuilder::new().with_title("A Pink Screen With a Moving Triangle");
     let cb = ContextBuilder::new();
     let display = Display::new(wb, cb, &event_loop).unwrap();
 
-    let vertex1 = Vertex { position: [ 0.0, 0.5] };
-    let vertex2 = Vertex { position: [ 0.5, -0.5] };
-    let vertex3 = Vertex { position: [ -0.5, -0.5] };
+    let vertex1 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.5, -0.5],
+    };
+    let vertex3 = Vertex {
+        position: [-0.5, -0.5],
+    };
     let shape = vec![vertex1, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-
     let vertex_shader_src = r#"
-    #version 140
+        #version 140
 
-    in vec2 position;
+        in vec2 position;
 
-    uniform float t;
+        uniform mat4 matrix;
 
-    void main() {
-        vec2 pos = position;
-        pos.x += t;
-        gl_Position = vec4(pos, 0.0, 1.0);
-    }
-"#;
+        void main() {
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
 
     let fragment_shader_src = r#"
         #version 140
@@ -50,24 +53,29 @@ pub fn create_moving_triangle(){
         }
     "#;
 
-    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program =
+        Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     let mut t: f32 = -0.5;
-    event_loop.run(move |ev, _, control_flow| {
-
-        match ev {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    *control_flow = ControlFlow::Exit;
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
-                },
+                }
                 _ => return,
             },
-            _ => (),
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
         }
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = ControlFlow::WaitUntil(next_frame_time);
 
         // we update `t`
@@ -78,9 +86,25 @@ pub fn create_moving_triangle(){
 
         let mut target = display.draw();
         target.clear_color(1.0, 0.0, 1.0, 1.0);
-        let uniforms = uniform! { t: t };
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                    &Default::default()).unwrap();
+
+        let uniforms = uniform! {
+            matrix: [
+                [ t.cos(), t.sin(), 0.0, 0.0],
+                [-t.sin(), t.cos(), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
         target.finish().unwrap();
     });
 }
